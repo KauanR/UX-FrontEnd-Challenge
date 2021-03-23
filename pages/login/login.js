97ec4c6614d7d75ea13ceb5e8d638d27cba3f060
Kauan Rocha <50336463+KauanR@users.noreply.github.com>
2021-03-23 15:50:37 -0300

    feat: login form implementation

---
+++ pages/login/login.js
@@ -0,0 +1,20 @@
+document.getElementById('submit-btn').addEventListener('click', event => {
+    event.preventDefault()
+
+    const USER = document.getElementById('user')
+    const PASSWORD = document.getElementById('password') 
+    const ERROR_MESSAGE = document.getElementById('error-message')
+
+    if(USER.value === 'email.usuario@compasso.com.br' && PASSWORD.value === '123456789') {
+        USER.classList.remove('invalid')
+        PASSWORD.classList.remove('invalid')
+        ERROR_MESSAGE.classList.add('hidden')
+        window.location.href = window.location.origin + '/UX-FrontEnd-Challenge/pages/home/home.html'
+    } else {
+        USER.classList.add('invalid')
+        PASSWORD.classList.add('invalid')
+        ERROR_MESSAGE.classList.remove('hidden')
+    }
+
+    console.log(window.location)
+})
