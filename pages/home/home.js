85b61a48e08d92827dff1e80cdf56076567baa4d
Kauan Rocha <50336463+KauanR@users.noreply.github.com>
2021-03-24 16:46:16 -0300

    feat: home header basic implementation

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,33 @@
-setInterval(() => this.refreshCounter(), 1000)
+const DAYS_OF_THE_WEEK = [
+    'domingo',
+    'segunda-feira',
+    'terça-feira',
+    'quarta-feira',
+    'sexta-feira',
+    'sábado'
+]
+
+const MONTHS = [
+    'janeiro',
+    'fevereiro',
+    'março',
+    'abril',
+    'maio',
+    'junho',
+    'julho',
+    'agosto',
+    'setembro',
+    'outubro',
+    'novembro',
+    'dezembro'
+]
+
+this.refreshHour()
+
+setInterval(() => {
+    this.refreshCounter()
+    this.refreshHour()
+}, 1000)
 
 function refreshCounter() {
     const COUNTER = document.getElementById('refresh-counter')
@@ -8,5 +37,16 @@ function refreshCounter() {
     else
         location.reload()
 
+}
+
+function refreshHour() {
+    const DATE = new Date()
+    
+    const HOUR_DISPLAY = document.getElementById('hour-display')
+    const DAY_DISPLAY = document.getElementById('day-display')
+
+    HOUR_DISPLAY.innerText = `${DATE.getHours()}:${DATE.getMinutes()}`
+
+    DAY_DISPLAY.innerText = `${DAYS_OF_THE_WEEK[DATE.getDay()]}, ${DATE.getUTCDate()} de ${MONTHS[DATE.getUTCMonth()]} de ${DATE.getUTCFullYear()}`
 }
 
