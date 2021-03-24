07b5f9e33fecfa8f60ba5284c78b55ff14d35892
Kauan Rocha <50336463+KauanR@users.noreply.github.com>
2021-03-24 16:16:59 -0300

    style: remove logs from home.js

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,8 +3,6 @@ setInterval(() => this.refreshCounter(), 1000)
 function refreshCounter() {
     const COUNTER = document.getElementById('refresh-counter')
 
-    console.log(COUNTER, COUNTER.innerText)
-
     if(COUNTER.innerText > 1)
         COUNTER.innerText = COUNTER.innerText - 1
     else
